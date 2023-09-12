511e41bd9d23387d9ebf88c3868daf19521e42f3
tv-tugova <116629172+tv-tugova@users.noreply.github.com>
2023-09-12 15:28:43 +0300

    Update script.js

--- pulse/js/script.js
+++ pulse/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="/pulse/icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="/pulse/icons/right.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="/pulse/icons/right.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
