670b7444d693b21e41ba9c82288f610281061875
tv-tugova <116629172+tv-tugova@users.noreply.github.com>
2023-09-13 14:51:03 +0300

    Update pulse after l. 71, contents of dist folder

--- pulse/js/script.js
+++ pulse/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="/pulse/icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="/pulse/icons/right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
